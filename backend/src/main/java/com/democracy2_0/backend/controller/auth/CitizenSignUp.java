42194bf85c20e48ad9b6e1fed6f6c49713927434
ErydCode <dyre.espana@gmail.com>
2023-09-03 13:00:28 +0200

    removed endpoints form CitizenSignUp.java and move to new Controller Class CheckUnique.java

--- backend/src/main/java/com/democracy2_0/backend/controller/auth/CitizenSignUp.java
+++ backend/src/main/java/com/democracy2_0/backend/controller/auth/CitizenSignUp.java
@@ -1,10 +1,7 @@
 package com.democracy2_0.backend.controller.auth;
 
 import com.democracy2_0.backend.configuration.JwtGenerator;
-import com.democracy2_0.backend.controller.citizen.CitizenRepository;
 import com.democracy2_0.backend.controller.citizen.NewCitizenSave;
-import com.democracy2_0.backend.controller.citizen.contact.MailRepository;
-import com.democracy2_0.backend.controller.citizen.contact.PhoneRepository;
 import com.democracy2_0.backend.data.citizen.Citizen;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -16,25 +13,7 @@ import org.springframework.web.bind.annotation.*;
 public class CitizenSignUp {
 
     private final NewCitizenSave newCitizenSave;
-    private final CitizenRepository citizenRepository;
-    private final MailRepository mailRepository;
-    private final PhoneRepository phoneRepository;
     private final JwtGenerator jwtGenerator;
-
-    @PostMapping("/username")
-    public boolean existsByUsername(@RequestBody String username) {
-        return citizenRepository.existsByUsername(username);
-    }
-
-    @PostMapping("/mail")
-    public boolean existsByMail(@RequestBody String mail) {
-        return mailRepository.existsByMail(mail);
-    }
-
-    @PostMapping("/phone")
-    public boolean existsByPhoneNumber(@RequestBody String phoneNumber) {
-        return phoneRepository.existsByPhoneNumber(phoneNumber);
-    }
 
     @PostMapping
     public Citizen save(@RequestBody Citizen citizen) {
